3efaaefd9d9a0d7832e488f301c783718df26d77
Vasily Petrunin <vasaaa7777@gmail.com>
2023-03-22 22:08:29 +0400

    add cycle, size new array

--- task/Program.cs
+++ task/Program.cs
@@ -1,4 +1,11 @@
 ﻿string[] FillArray (string[] array, int sizeString)
 {
-    
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= sizeString)
+        {
+            count++;
+        }
+    }
 }
