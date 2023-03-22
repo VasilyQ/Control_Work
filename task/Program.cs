dbae13e229ff3a91894dc1feece2d3a7dc221254
Vasily Petrunin <vasaaa7777@gmail.com>
2023-03-22 22:10:14 +0400

    add new array, fill new array

--- task/Program.cs
+++ task/Program.cs
@@ -8,4 +8,15 @@
             count++;
         }
     }
+    string[] newArray = new string[count];
+    int index = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+       if(array[i].Length <= sizeString)
+        {
+            newArray[index] = array[i];
+            index++;
+        } 
+    }
 }
